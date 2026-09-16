6d3631cf8ee11c7329c70479615f42ec9bd4ffd8
DMartins21 <davi_martins2009@outlook.com>
2026-09-16 15:24:29 -0300

    ...

--- backend/myfilms/DTOs/TokenModelDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace myfilms.DTOs;
-
-public class TokenModelDTO
-{
-    public string? AccessToken { get; set; }
-    public string? RefreshToken { get; set; }
-}
--- backend/myfilms/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace myfilms.Models;
-
-public class User : IdentityUser
-{
-    public string? RefreshToken { get; set; }
-    public DateTime RefreshTokenExpires { get; set; }
-}
